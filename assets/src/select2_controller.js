404afc240be50f8c1d84dc10b8ec95003c692fff
amorebietakoUdala@github.com <informatika@amorebieta.eus>
2024-02-07 13:01:30 +0100

    Added autofocus on select2 dropdown

--- assets/src/select2_controller.js
+++ assets/src/select2_controller.js
@@ -9,7 +9,9 @@ import 'select2/dist/js/i18n/es';
 import 'select2/dist/js/i18n/eu';
 
 class select2 extends Controller {
-   
+   static values = {
+      autofocus: { type: Boolean, default: true },
+   };   
    locale = null;
    connect() {
       this.locale = global.locale ?? document.getElementsByTagName("html")[0].getAttribute('lang');
@@ -18,6 +20,13 @@ class select2 extends Controller {
          language: this.locale,
       }
       $(this.element).select2(options);
+      if ( this.autofocusValue ) {
+         $(this.element).on('select2:open',this.onOpening);
+      }
+   }
+
+   onOpening(e) {
+      document.querySelector('.select2-search__field').focus();
    }
 }
 
